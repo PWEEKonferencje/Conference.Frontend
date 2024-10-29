e13a0f26a48c4dba25e46603e456f458318a273b
aleksanderpiluk <94018773+aleksanderpiluk@users.noreply.github.com>
2024-10-29 22:50:30 +0100

    add more recommended eslint/tslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import react from "eslint-plugin-react";
 import tseslint from "typescript-eslint";
 import prettierConfig from "eslint-config-prettier";
 
@@ -10,17 +11,24 @@ export default tseslint.config(
   {
     extends: [
       js.configs.recommended,
-      ...tseslint.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
       prettierConfig,
     ],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+        tsconfigRootDir: import.meta.dirname
+      }
     },
+    settings: { react: {version: "18.3"} },
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
+      react,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -28,6 +36,8 @@ export default tseslint.config(
         "warn",
         { allowConstantExport: true },
       ],
+      ...react.configs.recommended.rules,
+      ...react.configs["jsx-runtime"].rules,
     },
   },
 );
